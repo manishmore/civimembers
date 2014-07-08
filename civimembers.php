4a9cdeec9f722f49b316449fbee240013bb291b9
moremanish <manish.more@webaccess.co.in>
2014-07-08 13:18:11 +0530

    events list changes

--- civimembers.php
+++ civimembers.php
@@ -1,27 +1,27 @@
-<?php
-  /*
-   Plugin Name: Civicrm Code Generator for civimembers or events
-   Description: Civicrm Members Or Events
-   program in a random language.
-   Version: 0.1
-   Author: manish
-   Author URI: example.com
-  */
+]<?php
+/*
+  Plugin Name: Civicrm Code Generator for civimembers or events
+  Description: Civicrm Members Or Events
+  program in a random language.
+  Version: 0.1
+  Author: manish
+  Author URI: example.com
+*/
 
-  /*
-  * 
-  * author@manish
-  */
+/*
+ *
+ * author@manish
+ */
 
 function civimembers_install() {
 
-   return ;
- }
-  register_activation_hook(__FILE__, 'civimembers_install');
+  return ;
+}
+register_activation_hook(__FILE__, 'civimembers_install');
 
 function civimembers_uninstall() {
 
- return ;
+  return ;
 }
 
 register_deactivation_hook(__FILE__, 'civimembers_uninstall');
@@ -29,47 +29,58 @@ register_deactivation_hook(__FILE__, 'civimembers_uninstall');
 
 function civimembers_get_civicrm_members($id) {
 
-     require_once "wp-content/plugins/civicrm/civicrm.settings.php";
+  require_once "wp-content/plugins/civicrm/civicrm.settings.php";
+  require_once 'CRM/Core/Config.php';
 
-     require_once 'CRM/Core/Config.php';
-    
      $params = array(
-         'version' => 3,
-         'page' => 'CiviCRM',
-         'q' => 'civicrm/ajax/rest',
-         'sequential' => 1,
-         'id' =>$id['id'],
-         );
+       'version' => 3,
+       'page' => 'CiviCRM',
+       'q' => 'civicrm/ajax/rest',
+       'sequential' => 1,
+       'id' =>$id['id'],
+     );
      $result = civicrm_api('Event', 'get', $params);
 
      return $result["values"][0]["description"];
-   }
-
-  /*
-   [civievents id="your Event id"]
-   call back function for add_shortcode
-  */
-
-  add_shortcode('civievents','civimembers_get_civicrm_members');
-  
-function civievents_get_list($limit){
-		$params = array(
-			'version' => 3,
-			'page' => 'CiviCRM',
-			'q' => 'civicrm/ajax/rest',
-			'sequential' => 1,
-			);
-		$result = civicrm_api('Event', 'get', $params);
-                  echo "<br/>";
-                  var_dump($result);
-    
 }
 
 /*
- [civieventlist limit=""]
- 
+  [civievents id="your Event id"]
+  call back function for add_shortcode
 */
+add_shortcode('civievents','civimembers_get_civicrm_members');
 
-add_shortcode('civievents','civievents_get_list');
+function civievents_get_list(){
+  require_once "wp-content/plugins/civicrm/civicrm.settings.php";
+  require_once 'CRM/Core/Config.php';
+
+	$params = array(
+    'version' => 3,
+    'page' => 'CiviCRM',
+    'q' => 'civicrm/ajax/rest',
+    'sequential' => 1,
+  );
+  $result = civicrm_api('Event', 'get', $params);
+  var_dump($result);
+die('sdsdsd');
+  $k = 0;
+  /* CRM_Core_Error::debug( '$result', $result ); */
+
+  while($result)
+    {
+      echo $list["$k"]['title'];
+      if($k=2){
+        break;
+      }
+      $k++;
+    }
+  //  return $result;
+}
+
+/*
+   [civieventlist limit=""]
+   call back function for add_shortcode for function civievents_get_list.
+ */
+ add_shortcode('civieventlist','civievents_get_list');
 
   ?>
